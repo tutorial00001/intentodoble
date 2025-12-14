31a911a96190f647ddb53c7b5f76a0c63a0e3c86
tutorial00001 <tutorialdeliser@gmail.com>
2025-12-13 21:56:39 -0500

    ultima

--- misitio/settings.py
+++ misitio/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'misitio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/db.sqlite3',
     }
 }
 
